c9d013ede23f782351973425ae59889efe1ffe99
Rafaela Luiz <rafasluize@gmail.com>
2024-05-24 17:29:37 -0300

    feat: increase test

--- src/components/page/MyFav/index.spec.tsx
+++ src/components/page/MyFav/index.spec.tsx
@@ -5,39 +5,28 @@ import { queryRender } from 'test-utils/render';
 import { screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import Myfav from '.';
 
-
-
 const mockedUsedNavigate = jest.fn();
 
 jest.mock('react-router-dom', () => ({
-   ...jest.requireActual('react-router-dom') as any,
+  ...(jest.requireActual('react-router-dom') as any),
   useNavigate: () => mockedUsedNavigate,
 }));
 
 describe('Myfav', () => {
   it('show list', async () => {
-    const {  debug, getByTestId } = queryRender(<Myfav />);
+    const { debug, getByTestId } = queryRender(<Myfav />);
 
-    expect(getByTestId('thead')).toBeInTheDocument()
+    expect(getByTestId('thead')).toBeInTheDocument();
 
+    const skeleton = getByTestId('skeleton');
+    await waitForElementToBeRemoved(skeleton);
 
-    const skeleton = getByTestId('skeleton')
-    await waitForElementToBeRemoved(skeleton)
-
-
-
-
-    
-    
     const artist = await screen.findByTestId('buttons-1212');
 
     expect(artist).toBeInTheDocument();
-      await waitFor(()=> {
+    // await waitFor(()=> {
 
-        debug()
-      })
-
-
-
+    //   debug()
+    // })
   });
 });
